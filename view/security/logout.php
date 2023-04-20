c125dc968deff4752e847cf8cbd3eeeedd8947d9
Mamath68 <mathieu.stamm@gmail.Com>
2023-04-20 11:29:53 +0200

    logout

--- view/security/logout.php
+++ view/security/logout.php
@@ -1,10 +1,9 @@
 <?php
-    session_start();
-    unset($_SESSION["authentication"]);
-    unset($_SESSION["auth_user"]);
+session_start();
+unset($_SESSION["authentication"]);
+unset($_SESSION["auth_user"]);
 
-    $_SESSION['message'] = "Logged Out Successfully";
-    header("Location: layout.php");
-    exit(0);
+$_SESSION['message'] = "Logged Out Successfully";
+header("Location: layout.php");
+exit(0);
 ?>
-
