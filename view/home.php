0df1d7dcc42e3e104d7ad736c15d851d78945cfd
Mamath68 <mathieu.stamm@gmail.Com>
2023-04-20 11:29:16 +0200

    style+accueil

--- view/home.php
+++ view/home.php
@@ -6,53 +6,10 @@
     ratione eum temporibus aliquid, facere rerum in laborum debitis labore aliquam ullam cumque.</p>
 
 <p>
-    <a href="/security/login.php">Se connecter</a>
+    <a href="view/security/login.php">Se connecter</a>
     <span>&nbsp;-&nbsp;</span>
-    <a href="/security/register.php">S'inscrire</a>
+    <a href="view/security/register.php">S'inscrire</a>
 </p>
-
-<div class="container mt-5">
-    <div class="row justify-content-center">
-        <div class="col-md-6">
-
-            <?php
-            // Your message code
-            if (isset($_SESSION['message'])) {
-                echo '<h4 class="alert alert-warning">' . $_SESSION['message'] . '</h4>';
-                unset($_SESSION['message']);
-            } // Your message code
-            ?>
-
-            <div class="card shadow">
-                <div class="card-body">
-                    <form action="/security/register.php" method="POST">
-                        <div class="mb-3">
-                            <label>Username</label>
-                            <input type="text" name="pseudo" required placeholder="Username" class="form-control">
-                        </div>
-                        <div class="mb-3">
-                            <label>Email</label>
-                            <input type="email" name="addresse_mail" required placeholder="Email" class="form-control">
-                        </div>
-                        <div class="mb-3">
-                            <label>Password</label>
-                            <input type="password" name="mot_passe" required placeholder="Password"
-                                class="form-control">
-                        </div>
-                        <div class="mb-3">
-                            <label>Confirm Password</label>
-                            <input type="password" name="mot_passe" required placeholder="Confirm Password"
-                                class="form-control">
-                        </div>
-                        <div class="mb-3">
-                            <input type="submit" name="register_btn" class="btn btn-primary" value="Enregistrer">
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
--- view/layout.php
+++ view/layout.php
@@ -9,7 +9,7 @@
         referrerpolicy="origin"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css"
         integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="public/css/style.css">
     <title>FORUM</title>
@@ -43,16 +43,16 @@
 
                         if (App\Session::getUser()) {
                             ?>
-                            <a href="/security/viewProfile.html"><span class="fas fa-user"></span>&nbsp;
+                            <a href="/security/viewProfile.php"><span class="fas fa-user"></span>&nbsp;
                                 <?= App\Session::getUser() ?>
                             </a>
-                            <a class="href" href="/security/logout.html">Déconnexion</a>
+                            <a class="href" href="view/security/logout.php">Déconnexion</a>
                             <?php
                         } else {
                             ?>
-                            <a class="href" href="./view/security/login.php">Connexion</a>
-                            <a class="href" href="./security/register.html">Inscription</a>
-                            <a class="href" href="./model/entities/Topic.php">la liste des topics</a>
+                            <a class="href" href="view/security/login.php">Connexion</a>
+                            <a class="href" href="view/security/register.php">Inscription</a>
+                            <a class="href" href="model/entities/Topic.php">la liste des topics</a>
                             <?php
                         }
 
